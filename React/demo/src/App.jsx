0d010eaaec9126fdf724e08d081f4b560b6dfde3
Aditya Patidar <adityapatidar25102002@gmail.com>
2025-02-20 12:45:33 +0530

    day4

---
+++ React/demo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App(){
+  return(
+    <>
+    <div className="container text-red-500 text-center">I am a react webpage</div>
+
+    </>
+
+
+
+  )
+}
+
+export default App;
--- javaScript/productWebsite/script.js
+++ javaScript/productWebsite/script.js
@@ -3,32 +3,36 @@ const URL = "./makeup.json";
 async function fetchitems() {
   const response = await fetch(URL);
   const data = await response.json();
-  const product = data.product;
- // console.log(response);
- // console.log(data);
- const container = document.getElementById("con");
+  //const makeups = data.makeup;
+  console.log(data.makeup);
 
-  product.array.forEach(element => {
+  // console.log(response);
+  // console.log(data);
+  const container = document.getElementById("con");
+
+  data.forEach((element) => {
+    const items = document.createElement("div");
     console.log(element);
-    const items=document.createElement("div");
-    product.innerhtml=`<div class="box w-25 justify-content-center border rounded shadow">
+
+    items.innerHTML = `<div class="box w-25  justify-content-center border rounded shadow m-3">
             <div class="image d-grid justify-content-center p-3">
-              <img src="${element.Image_link}" alt="" />
+              <img src="${element.image_link}" alt="${element.name}" height="100" width="100" />
             </div>
-            <div class="name d-grid justify-content-center p-3">
+            <div class="name d-grid justify-content-center ">
+              <label for="brand">${element.brand}</label>
+              
+            </div>
+            <div class="name d-grid justify-content-center p-2 ">
               <label for="name">${element.name}</label>
               
             </div>
             <div class="price d-grid justify-content-center p-3">
               <label for="price">${element.price}</label>
             </div>
-          </div>`
+          </div>`;
 
-
+    container.appendChild(items);
   });
-
-container.appendChild("items")
-  
 }
 
-fetchitems()
+fetchitems();
